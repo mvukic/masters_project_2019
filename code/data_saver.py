e3961bc6944e81c33bd36c10345caf6420b03b6f
mvukic <1702367+mvukic@users.noreply.github.com>
2019-05-25 15:15:28 +0100

    update

--- code/data_saver.py
+++ code/data_saver.py
@@ -8,33 +8,38 @@ class DataSaver:
 
   def __init__(self):
     # Point clouds directory
-    self.point_clouds_path = 'output/point_clouds'
+    self.pc_path = 'output/point_clouds'
     # Actor transforms directory
-    self.actor_transforms_path = 'output/actor_transforms'
+    self.trans_path = 'output/actor_transforms'
     # Relative transform path
     self.lidar_relative_transform_path = 'output/relative_transform.txt'
 
   def initialize_folders(self):
-    utils.create_directory(self.actor_transforms_path)
-    utils.create_directory(self.point_clouds_path)
+    utils.create_directory(self.trans_path)
+    utils.create_directory(self.pc_path)
 
   def save(self, scans):
     self.initialize_folders()
-    self.save_LIDAR_to_actor_relative_transform()
+    self.save_rel_trans()
     start_time = time.time()
 
     with ThreadPoolExecutor(max_workers=10) as executor:
-      jobs = [executor.submit(self.process_pair, scan, transform, index) for index, (scan, transform) in enumerate(scans[1:])]
+      jobs = list()
+      for index, (scan, transform) in enumerate(scans[1:]):
+        job = executor.submit(self.process_pair, scan, transform, index)
+        jobs.append(job)
       for future in as_completed(jobs):
         future.result()
     print(f"\tSaved {len(scans)-1} scan(s)")
     print(f"\tTime elapsed {time.time() - start_time}")
 
   def process_pair(self, scan, transform, index):
-    self.save_scan(scan, f'{self.point_clouds_path}/{scan.frame_number:06d}.ply', index + 1)
-    self.save_transform(transform, scan.timestamp, f'{self.actor_transforms_path}/{scan.frame_number:06d}.txt', index + 1)
+    scan_path = f'{self.pc_path}/{scan.frame_number:06d}.ply'
+    self.save_scan(scan, scan_path, index + 1)
+    trans_path =  f'{self.trans_path}/{scan.frame_number:06d}.txt'
+    self.save_transform(transform, scan.timestamp, trans_path, index + 1)
 
-  def save_LIDAR_to_actor_relative_transform(self):
+  def save_rel_trans(self):
     with open(self.lidar_relative_transform_path, 'w+') as file:
       file.write(utils.transform_to_string(LIDARProperties.lidar_relative_postion))
 
