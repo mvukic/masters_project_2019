f5f69274537492e5b9562cd8c1dd37cb0be11e0b
mvukic <1702367+mvukic@users.noreply.github.com>
2019-05-26 14:25:04 +0200

    add c

---
+++ projects/pcl/build/CMakeFiles/pcl_test_bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pcl_test_bin.dir/pcl_test.cpp.o"
+  "pcl_test_bin.pdb"
+  "pcl_test_bin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pcl_test_bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
